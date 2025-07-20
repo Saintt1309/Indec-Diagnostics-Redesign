a8e9b546684449f4ce68bc3f8957c23963a9f66f
Saintt1309 <30825508+Saintt1309@users.noreply.github.com>
2025-07-21 04:35:20 +0800

    Added first page, footer, and fixed some bugs

---
+++ index.php
@@ -0,0 +1,91 @@
+<?php
+include 'header.html'
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8" />
+    <title>Indec Diagnostics - Home</title>
+    <link rel="stylesheet" href="styles/index.css">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:ital,wght@0,100..700;1,100..700&family=Manrope:wght@200..800&family=Source+Sans+3:ital,wght@0,200..900;1,200..900&display=swap" rel="stylesheet">
+
+</head>
+<body>
+    <section class="hero-section">
+        <img src="images/main-hero-image.png">
+        <h1>Leading The World's Immunology Research</h1>
+        <p>We are dedicated to advancing the science of immunology through pioneering research and innovation. By uncovering the complexities of the immune system, we aim to drive transformative breakthroughs in medicine. Our work powers the future of global health and disease prevention.</p>
+    </section>
+
+    <section class="product-tease">
+        <h1>Scientifically Proven, Widely Used</h1>
+        <p>Our most in-demand diagnostics deliver reliable results when time and precision matter most—used by healthcare providers and researchers around the world.</p>
+        <div class="product-card-container">
+            <div class="product-card">
+                <img src="images/Virocheck-HIV-12 1.png">
+                <h2>Virocheck HIV 1/2</h2>
+                <p>Catalog Number: RV-H10A / RV-H25A</p>
+                <p>Package Type: 10 / 25</p>
+                <div class="product-card-button">
+                    <a href="#">See More</a>
+                </div>
+            </div>
+
+            <div class="product-card">
+                <img src="images/Plasmotec-Malaria-PfPv 1.png">
+                <h2>Plasmotec Malaria Pf/Pv</h2>
+                <p>Catalog Number: RP-PV10 / RP-PV25</p>
+                <p>Package Type: 10 / 25</p>
+                <div class="product-card-button">
+                    <a href="#">See More</a>
+                </div>
+            </div>
+
+            <div class="product-card">
+                <img src="images/ID-Biosens-Dengue-NS1 1.png">
+                <h2>ID Biosens Dengue NS1</h2>
+                <p>Catalog Number: RV-D02</p>
+                <p>Package Type: 25</p>
+                <div class="product-card-button">
+                    <a href="#">See More</a>
+                </div>
+            </div>
+        </div>
+    </section>
+        <section class="about-section">
+            <div class="process-1">
+                <img id="process-img" src="images/process1.png">
+                <div class="process-description">
+                    <h1>Engineered for Results</h1>
+                    <p>Driven by science and precision, our process takes discoveries from concept to impact. Advanced methods and strict validation power every breakthrough.</p>
+                </div>
+                <img id="process-icon" src="icons/process1.svg">
+            </div>
+
+            <div class="process-2">
+                <img id="process-icon" src="icons/process2.svg">
+                <div class="process2-description">
+                    <h1>Standards That Pave the Future</h1>
+                    <p>Quality drives every stage of our work. Our strict controls ensure reliable, reproducible research that meets the highest scientific and regulatory standards.</p>
+                </div>
+                <img id="process-img" src="images/process2.png">
+            </div>
+
+            <div class="process-3">
+                <img id="process-img" src="images/process3.png">
+                <div class="process-description">
+                    <h1>Delivering Innovation Worldwide</h1>
+                    <p>We manage end-to-end distribution with precision, from lab to destination. Every delivery is handled with care to maintain quality, compliance, and global reach.</p>
+                </div>
+                <img id="process-icon" src="icons/process3.svg">
+            </div>
+    </section>
+</body>
+<?php
+include 'footer.html'
+?>
+</html>
